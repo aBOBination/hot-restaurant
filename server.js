9dbe59ccc0604d78d517622c3173328f637ea7eb
Robert Marks <robert.marks54@gmail.com>
2020-05-16 10:58:34 -0600

    rebase

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var reservations = [
     customerName: 'obiwankenobi',
     phoneNumber: '123-456-7890',
     customerEmail: 'hellothere@highground.com',
-    customerID: 55,
+    customerID: 'obiwan1',
   },
 ];
 
@@ -94,17 +94,19 @@ app.get('/api/characters/villians/:character', function (req, res) {
 app.post('/api/tables', function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  var newTable = req.body;
+  var newReservation = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newTable.routeName = newTable.name.replace(/\s+/g, '').toLowerCase();
+  newReservation.routeName = newReservation.name
+    .replace(/\s+/g, '')
+    .toLowerCase();
 
-  console.log(newTable);
+  console.log(newReservation);
 
-  heros.push(newTable);
+  heros.push(newReservation);
 
-  res.json(newTable);
+  res.json(newReservation);
 });
 
 app.post('/api/characters/villians', function (req, res) {
